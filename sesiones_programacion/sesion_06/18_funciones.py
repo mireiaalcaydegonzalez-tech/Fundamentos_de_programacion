e31edc0cc9fcdbd371ab353871c60686f6c4ca32
Origin <mireiaalcaydegonzalez@gmail.com>
2025-11-27 12:04:26 +0100

    ejercicio 18 sesion 6

---
+++ sesiones_programacion/sesion_06/18_funciones.py
@@ -0,0 +1,11 @@
+# Funcion saludo
+def saludo(nombre):
+    print("-"*10)
+    print(f"Bienvenido a la matrix {nombre}")
+    print("-"*10)
+
+# Usar la funcion
+saludo("Paco")
+saludo("Lucia")
+saludo("Maria")
+

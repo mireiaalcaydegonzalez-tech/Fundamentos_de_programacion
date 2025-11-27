fa2bc84dbd345884a917547d1c34b3c95080bee3
Origin <mireiaalcaydegonzalez@gmail.com>
2025-11-27 12:26:30 +0100

    ejercicio 18 sesion 6

---
+++ sesiones_programacion/sesion_07/19_ejemplo_funciones.py
@@ -0,0 +1,21 @@
+# Funcion sin parametros
+def saludar():
+    print("Hola ¿como estas?")
+
+# Difinir una funcion que realice una suma
+def suma(a,b):
+    print(a+b)
+
+def suma_return(a,b):
+    print(a+b)
+    return a+b
+
+
+# Uso de las funciones
+saludar()
+suma(3,2)
+
+resultado = suma_return(4, 20)
+
+PRINT("Fin del codigo")
+

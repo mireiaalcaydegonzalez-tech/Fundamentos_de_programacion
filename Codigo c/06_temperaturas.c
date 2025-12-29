aa0bac9c6ac3646fdf414fc7f479aa22c158ce9d
Origin <mireiaalcaydegonzalez@gmail.com>
2025-12-29 23:32:29 +0100

    Constantes, for y while sesion 8

--- Codigo c/06_temperaturas.c
+++ Codigo c/06_temperaturas.c
@@ -4,33 +4,26 @@
 #include <time.h>
 
 int main() {
-	// Semilla del generador de números aleatorios
-	srand((unsigned)time(NULL));
+	//Inicializar la semilla para números aleatorios
+	srand(time(NULL));
+	int temperatura = rand() % 46; // Numero aleatorio entre 0 y 45 (ambos inclusive)
+    printf("Temperatura generada: %d\n", temperatura);
 
-	// Temperatura entre 0 y 45 (ambos inclusive)
-	int temp = rand() % 46;
-
-	printf("Temperatura generada: %d\n", temp);
-
-	switch (temp) {
-		case 0: case 1: case 2: case 3: case 4:
-		case 5: case 6: case 7: case 8: case 9: case 10:
+    // Evaluar la temperatura usando switch
+	switch (temperatura / 10) { // Dividir por 10 para agrupar en rangos de 10  
+		case 0: 
 			printf("Hace mucho frío\n");
 			break;
-		case 11: case 12: case 13: case 14: case 15:
-		case 16: case 17: case 18: case 19: case 20:
+		case 1:
 			printf("Hace fresquito\n");
 			break;
-		case 21: case 22: case 23: case 24: case 25:
-		case 26: case 27: case 28: case 29: case 30:
+		case 2:
 			printf("No se está mal\n");
 			break;
-		case 31: case 32: case 33: case 34: case 35:
-		case 36: case 37: case 38: case 39: case 40:
+		case 3:
 			printf("Comienza a hacer calor\n");
 			break;
 		default:
-			// Incluye 41-45, y el caso teórico de valores mayores de 45
 			printf("Muero achicharrado\n");
 			break;
 	}
